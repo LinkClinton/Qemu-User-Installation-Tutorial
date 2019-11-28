2d10496cab5edc581b956442f6cfd5e5296b3a99
LinkClinton <LinkClinton@outlook.com>
2019-11-28 22:33:59 +0800

    Reset Sources.list Issue #1

--- file/lazy-master.sh
+++ file/lazy-master.sh
@@ -1,3 +1,4 @@
+sudo cp /etc/apt/sources.list /etc/apt/sources_backup.list;
 sudo rm /etc/apt/sources.list;
 sudo cp sources.list /etc/apt;
 sudo apt-get update;
@@ -7,4 +8,7 @@ sudo apt-get install libc6-mipsel-cross;
 sudo mkdir /etc/qemu-binfmt;
 sudo ln -s /usr/mipsel-linux-gnu /etc/qemu-binfmt/mipsel;
 sudo cp -r mipsel-gdb /usr/local/;
+sudo rm /etc/apt/sources.list
+sudo cp /etc/apt/sources_backup.list /etc/apt/sources.list
+sudo rm /etc/apt/sources_backup.list
 wait
